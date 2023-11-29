b3f039cec81560da87ac28ac304c7952d0caf775
sahinaykkt <sahinaykkt@gmail.com>
2023-11-30 01:25:44 +0300

    Circle with module

---
+++ basics/module/circle.js
@@ -0,0 +1,7 @@
+const circleArea = (radius) =>  Math.round(Math.PI * (radius * radius));
+const circleCircumference = (radius) =>  Math.round(2 * Math.PI * radius);
+
+module.exports = {
+    circleArea,
+    circleCircumference
+};
