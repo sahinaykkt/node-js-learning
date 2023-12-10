49b5674717b2e2be65a8b225c038d70f3a030964
sahinaykkt <sahinaykkt@gmail.com>
2023-12-10 03:10:30 +0300

    File Changes

--- basics/module/circle.js
+++
@@ -1,7 +0,0 @@
-const circleArea = (radius) =>  Math.round(Math.PI * (radius * radius));
-const circleCircumference = (radius) =>  Math.round(2 * Math.PI * radius);
-
-module.exports = {
-    circleArea,
-    circleCircumference
-};
--- basics/module/index.js
+++
@@ -1,4 +0,0 @@
-const { circleArea, circleCircumference } = require('./circle');
-
-console.log(circleArea(5));
-console.log(circleCircumference(5));
